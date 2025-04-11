aa60b06c694b037b4ec73a834c9a76b9a26a8863
dbc2201 <divyanshb22@gmail.com>
2025-04-12 02:05:07 +0530

    create a product not found exception

---
+++ src/main/java/io/github/dbc2201/spring/boot/demos/shopkart/product/ProductNotFoundException.java
@@ -0,0 +1,16 @@
+package io.github.dbc2201.spring.boot.demos.shopkart.product;
+
+/**
+ * Exception thrown when a product is not found in the database.
+ * Used when searching for a product by id or name.
+ */
+public class ProductNotFoundException extends RuntimeException {
+
+	/**
+	 * Creates a new ProductNotFoundException with the specified message.
+	 * @param message The error message explaining why the product was not found.
+	 */
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
+}
