fd8163763348889e0ac2ac0fb371118c1e0a4a67
dbc2201 <divyanshb22@gmail.com>
2025-04-12 02:10:28 +0530

    add exceptions to method signatures

--- src/main/java/io/github/dbc2201/spring/boot/demos/shopkart/product/ProductService.java
+++ src/main/java/io/github/dbc2201/spring/boot/demos/shopkart/product/ProductService.java
@@ -3,21 +3,21 @@ package io.github.dbc2201.spring.boot.demos.shopkart.product;
 import java.util.List;
 
 public interface ProductService {
-	Product createProduct(Product product);
+	Product createProduct(Product product) throws ProductValidationException;
 
-	List<Product> getAllProducts();
+	List<Product> getAllProducts() throws ProductNotFoundException;
 
-	Product getProductById(long id);
+	Product getProductById(long id) throws ProductNotFoundException;
 
-	Product getProductByName(String name);
+	Product getProductByName(String name) throws ProductNotFoundException;
 
-	Product updateProductPrice(long id, double newPrice);
+	Product updateProductPrice(long id, double newPrice) throws ProductValidationException;
 
-	Product updateProductName(long id, String newName);
+	Product updateProductName(long id, String newName) throws ProductValidationException;
 
-	Product updateProductDescription(long id, String newDescription);
+	Product updateProductDescription(long id, String newDescription) throws ProductValidationException;
 
-	Product updateProductImageUrl(long id, String newImageUrl);
+	Product updateProductImageUrl(long id, String newImageUrl) throws ProductValidationException;
 
-	boolean deleteProduct(long id);
+	boolean deleteProduct(long id) throws ProductNotFoundException;
 }
